34592fde4d56925595bd4146ebe553a4bfd38e7f
vincent178 <vincent.007.cn@gmail.com>
2017-03-23 08:27:31 +0800

    init project

---
+++ src/index.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import thunk from 'redux-thunk';
+import { Provider } from 'react-redux';
+import { createStore, applyMiddleware } from 'redux';
+import App from './App';
+import './index.css';
+import reducers from './reducers';
+
+const store = createStore(
+  reducers,
+  applyMiddleware(thunk)
+);
+
+ReactDOM.render(
+  <Provider store={store} >
+    <App />
+  </Provider>,
+  document.getElementById('root')
+);
---
+++ src/reducers/gallery.js
@@ -0,0 +1,12 @@
+
+const initialState = {
+  items: []
+};
+
+
+export default function galleryReducer(state = initialState, action) {
+  switch(action.type) {
+    default:
+      return state;
+  }
+}
