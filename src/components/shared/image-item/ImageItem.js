3c4e0874d273f5abea61d28496ba2ad3d8672a98
vincent178 <vincent.007.cn@gmail.com>
2017-04-04 10:55:26 +0800

    update

--- src/components/shared/image-item/ImageItem.js
+++ src/components/shared/image-item/ImageItem.js
@@ -1,4 +1,5 @@
 import React, {Component, PropTypes} from "react";
+import {findDOMNode} from 'react-dom';
 import CloseIcon from "../icons/close-icon/CloseIcon";
 import "./style.css";
 import image from './s1.jpg';
@@ -10,20 +11,98 @@ export default class ImageItem extends Component {
     image: PropTypes.string
   };
 
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      isPressed: false,
+      mouseXY: [0, 0],
+      mouseXYDelta: [0, 0]
+    }
+  }
+
+  componentDidMount() {
+    const node = findDOMNode(this);
+
+    node.addEventListener('touchstart', this.handleTouchStart.bind(this));
+    node.addEventListener('touchend', this.handleTouchEnd.bind(this));
+
+    node.addEventListener('mousedown', this.handleMouseDown.bind(this));
+    node.addEventListener('mouseup', this.handleMouseUp.bind(this));
+
+    node.addEventListener('touchmove', this.handleTouchMove.bind(this));
+    node.addEventListener('mousemove', this.handleMouseMove.bind(this));
+
+  }
+
+  handleTouchStart(e) {
+    this.handleMouseDown(e);
+  }
+
+  handleTouchMove(e) {
+    this.handleMouseMove(e);
+  }
+
+  handleTouchEnd(e) {
+    this.handleMouseUp(e);
+  }
+
+  handleMouseDown(e) {
+    console.log('starting!');
+
+    console.log(e);
+
+    const touch = e.touches[0];
+
+    this.setState({
+      isPressed: true,
+      mouseXY: [touch.pageX, touch.pageY]
+    });
+  }
+
+  handleMouseMove(e) {
+    console.log(e);
+    const { mouseXY, mouseXYDelta } = this.state;
+    const touch = e.touches[0];
+
+    console.log(touch.pageX);
+
+    this.setState({
+      mouseXYDelta: [touch.pageX - mouseXY[0], touch.pageY - mouseXY[1]]
+    })
+  }
+
+  handleMouseUp() {
+    console.log('ending');
+    this.setState({
+      isPressed: false,
+      mouseXY: [0, 0],
+      mouseXYDelta: [0, 0]
+    });
+  }
+
+
+
   render() {
     const imageBackup = "https://scontent-sit4-1.cdninstagram.com/t51.2885-15/e15/10919269_1573513876220806_393752427_n.jpg";
     
     const imageStyle = {
       backgroundImage: `url(${image})`
     };
+
+    const { mouseXYDelta, isPressed } = this.state;
     
     return (
-      <div className="image-container m-image-box">
-        {/*<img src={image} />*/}
+      <div className="image-container m-image-box" style={{
+        transform: `translate(${mouseXYDelta[0]}px, ${mouseXYDelta[1]}px) scale(${isPressed ? 1.1 : 1.0})`,
+        zIndex: isPressed ? 99 : 1
+      }}>
+
         <div className="image-item" style={imageStyle}></div>
         <div className="m-box-center m-box-center-a close-button">
           <CloseIcon size={24} />
         </div>
+
       </div>
     );
   }
--- src/components/shared/image-list/ImageList.js
+++ src/components/shared/image-list/ImageList.js
@@ -1,153 +1,31 @@
-import React, {Component} from 'react';
-import {Motion, spring} from 'react-motion';
+import React, {Component} from "react";
+import {Motion, spring} from "react-motion";
 import ImageItem from "../image-item/ImageItem";
-import ImageUpload from "../image-upload/ImageUpload";
-import {range} from 'lodash';
 import "./style.css";
-
-function reinsert(arr, from, to) {
-  const _arr = arr.slice(0);
-  const val = _arr[from];
-  _arr.splice(from, 1);
-  _arr.splice(to, 0, val);
-  return _arr;
-}
-
-
-function clamp(n, min, max) {
-  return Math.max(Math.min(n, max), min);
-}
-
-const springConfig = {stiffness: 300, damping: 50};
-const itemCount = 4;
-
 
 export default class ImageList extends Component {
   
   constructor(props) {
     super(props);
-    
+
     this.state = {
-      topDeltaY: 0,
-      mouseY: 0,
-      isPressed: false,
-      originalPosOfLastPressed: 0,
-      order: range(itemCount)
+      mouseXY: [0, 0],
+      mouseCircleDelta: [0, 0],
+      lastPress: null,
+      isPressed: false
     }
-  }
-  
-  
-  componentDidMount() {
-    window.addEventListener('touchmove', this.handleTouchMove.bind(this));
-    window.addEventListener('touchend', this.handleMouseUp.bind(this));
-    window.addEventListener('mousemove', this.handleMouseMove.bind(this));
-    window.addEventListener('mouseup', this.handleMouseUp.bind(this));
-    window.addEventListener('resize', this.handleResize.bind(this));
-  }
-
-  handleResize(e) {
-    console.log('this is resizing window');
-  }
-
-  handleTouchStart(key, pressLocation, e) {
-    this.handleMouseDown(key, pressLocation, e.touches[0]);
-  }
-
-  handleTouchMove(e) {
-    e.preventDefault();
-    this.handleMouseMove(e.touches[0]);
-  }
-
-
-  handleMouseDown(pos, pressY, {pageX, pageY}) {
-    console.log("FFFFFFFFFFFF");
-    this.setState({
-      topDeltaX: pageY - pressY,
-      mouseY: pressY,
-      isPressed: true,
-      originalPosOfLastPressed: pos
-    });
-  }
-
-  handleMouseMove({pageX, pageY}) {
-    const {isPressed, topDeltaY, order, originalPosOfLastPressed} = this.state;
-
-    if (isPressed) {
-      const mouseY = pageY - topDeltaY;
-      const currentColumn = clamp(Math.round(mouseY / 100), 0, itemCount - 1);
-
-      let newOrder = order;
-
-      if (currentColumn !== order.indexOf(originalPosOfLastPressed)) {
-        newOrder = reinsert(order, order.indexOf(originalPosOfLastPressed), currentColumn);
-      }
-
-      this.setState({mouseY, order: newOrder});
-    }
-  }
-
-  handleMouseUp() {
-    this.setState({isPressed: false, topDeltaY: 0});
   }
 
   render() {
-    const {mouseY, isPressed, originalPosOfLastPressed, order} = this.state;
 
     return (
       <div className="image-list">
-        {/*{range(itemCount).map(i => {*/}
-
-          {/*const style = originalPosOfLastPressed === i && isPressed ?*/}
-            {/*{*/}
-              {/*scale: spring(1.1, springConfig),*/}
-              {/*shadow: spring(1.6, springConfig),*/}
-              {/*y: mouseY*/}
-            {/*} :*/}
-            {/*{*/}
-              {/*scale: spring(1, springConfig),*/}
-              {/*shadow: spring(1, springConfig),*/}
-              {/*y: spring(order.indexOf(1) * 100, springConfig)*/}
-            {/*};*/}
-
-          {/*return (*/}
-            {/*<Motion style={style} key={i}>*/}
-              {/*{*/}
-                {/*({scale, shadow, y}) =>*/}
-                  {/*<div*/}
-                    {/*onMouseDown={this.handleMouseDown.bind(this, i, y)}*/}
-                    {/*onTouchStart={this.handleTouchStart.bind(this, i, y)}*/}
-                    {/*style={{*/}
-                      {/*boxShadow: `rgba(0, 0, 0, 0.2) 0px ${shadow}px ${2 * shadow}px 0px`,*/}
-                      {/*transform: `translate3d(0, ${y}px, 0) scale(${scale})`,*/}
-                      {/*WebkitTransform: `translate3d(0, ${y}px, 0) scale(${scale})`,*/}
-                      {/*zIndex: i === originalPosOfLastPressed ? 99 : i*/}
-                    {/*}}>*/}
-                    {/*<ImageItem />*/}
-                  {/*</div>*/}
-              {/*}*/}
-
-            {/*</Motion>*/}
-          {/*)*/}
-
-        {/*})}*/}
 
         <ImageItem/>
         <ImageItem/>
         <ImageItem/>
         <ImageItem/>
         <ImageItem/>
-        <ImageItem/>
-        <ImageItem/>
-        <ImageItem/>
-        <ImageItem/>
-        <ImageItem/>
-        <ImageItem/>
-        <ImageItem/>
-        <ImageItem/>
-        <ImageItem/>
-
-
-        {/*<ImageUpload/>*/}
       </div>
     );
   }
