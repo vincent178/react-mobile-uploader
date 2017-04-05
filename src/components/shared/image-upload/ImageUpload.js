b08974a7731486bbd637a742dda47319f8f86759
vincent178 <vincent.007.cn@gmail.com>
2017-04-05 18:32:36 +0800

    update

--- src/components/shared/image-upload/ImageUpload.js
+++ src/components/shared/image-upload/ImageUpload.js
@@ -3,14 +3,28 @@ import './style.css';
 
 
 export default class ImageUpload extends Component {
-
+  
+  constructor(props) {
+    
+    super(props);
+    
+    this.handleUploadFiles = this.handleUploadFiles.bind(this);
+  }
+  
+  handleUploadFiles(e) {
+    const files = e.target.files;
+    console.log(files);
+  }
 
   render() {
-
     return (
       <div className="m-uploader m-square-box m-small-radius">
-        <input type="file"
-               accept="image/jpg,image/jpeg,image/png,image/gif" />
+        <input
+          type="file"
+          accept="image/jpg,image/jpeg,image/png,image/gif"
+          multiple="multiple"
+          onChange={this.handleUploadFiles}
+        />
       </div>
     );
   }
